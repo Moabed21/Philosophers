47ffb3b868352bffc66495205eda3414229870e3
Moabed21 <moathabed21@gmail.com>
2026-02-15 23:40:33 +0300

    cleaner built successfully

--- philo/routine.c
+++ philo/routine.c
@@ -3,43 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: moabed <moabed@student.42amman.com>        +#+  +:+       +#+        */
+/*   By: moabed <moabed@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 20:01:33 by moabed            #+#    #+#             */
-/*   Updated: 2026/02/14 19:58:52 by moabed           ###   ########.fr       */
+/*   Updated: 2026/02/15 23:39:27 by moabed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	hold_fork_and_eat(t_pcard *philo, int p_num)
+{
+
+}
 void	*routine(void *p)
 {
+	int			i;
 	t_pcard		*philo;
 	long long	timestamp;
 
 	philo = (t_pcard *)p;
 	timestamp = time_calc();
 	printf("%lld %d is thinking\n", timestamp, philo->pdetails.pnumber);
-	hold_fork(&philo, philo->pdetails.pnumber);
+	hold_fork_and_eat(philo, philo->pdetails.pnumber);
+	while (1)
+	{
+	}
 	return (p);
 }
-
-void	hold_fork(t_pcard **philo, int p_num)
+void	mess_clean(t_pcard *philos, int pnum, pthread_mutex_t *forks)
 {
-	if (p_num % 2 == 0)
+	int	i;
+
+	//allocated elements are : print_mic , forks, philosophers
+	i = 0;
+	//first target to be freed : print_mic
+	pthread_mutex_destroy(philos->print_mic);
+	free(philos->print_mic);
+	//then, the forks starting form the right
+	while (i < pnum)
 	{
-		if (pthread_mutex_lock((*philo)->left_fork) != 0)
-		{
-		}
+		pthread_mutex_destroy(&forks[i]);
+		i++;
 	}
-	if (p_num % 3 == 0)
-	{
-		if (pthread_mutex_lock((*philo)->right_fork) != 0)
-		{
-		}
-	}
+	free(forks);
+	//finally, the table godfather :)
+	free(philos);
 }
-void	routine_start(t_pcard *philos, int pnum)
+
+//create two mics (pointers) for the death second for the printing the messages
+int	routine_start(t_pcard *philos, int pnum, pthread_mutex_t *forks)
 {
 	int	i;
 
@@ -48,6 +61,8 @@ void	routine_start(t_pcard *philos, int pnum)
 	{
 		if (pthread_create(&philos[i].thread, NULL, &routine, &philos[i]) != 0)
 		{
+			mess_clean(philos, pnum, forks);
+			return (1);
 		}
 		i++;
 	}
@@ -56,13 +71,13 @@ void	routine_start(t_pcard *philos, int pnum)
 	{
 		if (pthread_join(philos[i].thread, NULL) != 0)
 		{
+			mess_clean(philos, pnum, forks);
+			return (1);
 		}
 		i++;
 	}
+	return (0);
 }
 // eat operation starts when the two forks (mutexs) are locked
 
-void	eat_operation(t_pcard **philos, pthread_mutex_t **forks)
-{
-	smart_sleep((*philos)->pdetails.tte,(*philos));
-}
+
