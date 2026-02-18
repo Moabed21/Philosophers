0722456952868215f944cdd8a94b3daec436b0a0
Moabed21 <moathabed21@gmail.com>
2026-02-18 23:54:42 +0300

    organizing functions

--- philo/utils2.c
+++ philo/utils2.c
@@ -6,38 +6,15 @@
 /*   By: moabed <moabed@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 22:06:44 by moabed            #+#    #+#             */
-/*   Updated: 2026/02/17 00:59:52 by moabed           ###   ########.fr       */
+/*   Updated: 2026/02/18 13:08:28 by moabed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-long long	time_calc(void)
-{
-	struct timeval	time;
-	long long		time_in_millisec;
-
-	gettimeofday(&time, NULL);
-	time_in_millisec = (time.tv_sec * 1000) + (time.tv_usec / 1000);
-	return (time_in_millisec);
-}
-
-void	smart_sleep(long long time, t_pcard *philo)
-{
-	long long	start_time;
-
-	start_time = time_calc();
-	while (time_calc() - start_time < time)
-	{
-		if ((*philo->is_dead) == 1)
-			break ;
-		usleep(250);
-	}
-}
-
 void	mess_clean(t_pcard *philos, int pnum, pthread_mutex_t *forks)
 {
-	int i;
+	int	i;
 
 	//allocated elements are : print_mic , forks, philosophers
 	i = 0;
@@ -52,6 +29,43 @@ void	mess_clean(t_pcard *philos, int pnum, pthread_mutex_t *forks)
 	}
 	free(forks);
 	//finally, the table godfather :)
-	free(philos);
 	free(philos->is_dead);
+	free(philos);
+}
+
+void	smart_sleep(long long time, t_pcard *philo)
+{
+	long long	start_time;
+
+	start_time = time_calc();
+	while (time_calc() - start_time < time)
+	{
+		if ((*philo->is_dead) == 1)
+			break ;
+		usleep(100);
+	}
+}
+
+long long	time_calc(void)
+{
+	struct timeval	time;
+	long long		time_in_millisec;
+
+	gettimeofday(&time, NULL);
+	time_in_millisec = (time.tv_sec * 1000) + (time.tv_usec / 1000);
+	return (time_in_millisec);
+}
+
+void	assign_forks(pthread_mutex_t *forks, t_pcard **philos,
+		int philos_number)
+{
+	int	i;
+
+	i = 0;
+	while (i < philos_number)
+	{
+		(*philos)[i].right_fork = &forks[(i) % philos_number];
+		(*philos)[i].left_fork = &forks[(i + 1) % philos_number];
+		i++;
+	}
 }
